55ac664e3adf6ac632ed7ceb3d130067c98ca3a9
Antonio Rengifo Daza <antoren9708@gmail.com>
2025-02-17 18:04:36 +0000

    Generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+      const suits = ['♥', '♠', '♣', '♦'];
+      const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+      
+      const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+      const randomValue = values[Math.floor(Math.random() * values.length)];
+      const cardElement = document.getElementById('card');
+      
+      document.getElementById('top-symbol').textContent = randomSuit;
+      document.getElementById('value').textContent = randomValue;
+      document.getElementById('bottom-symbol').textContent = randomSuit;
+
+      if (randomSuit === '♥' || randomSuit === '♦') {
+        cardElement.classList.add('red');
+        cardElement.classList.remove('black');
+      } else {
+          cardElement.classList.add('black');
+          cardElement.classList.remove('red');
+      }
+
 };
